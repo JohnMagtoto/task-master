4e9303ac848264a0254fa58e2184a5486d3bf7af
JohnMagtoto <johnmagtoto@gmail.com>
2017-08-18 11:25:46 +0900

    added major component blocks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { DateComponent } from './main/date/date.component';
+import { ProgressComponent } from './main/progress/progress.component';
+import { TaskListComponent } from './main/task-list/task-list.component';
+import { TaskItemComponent } from './main/task-list/task-item/task-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    DateComponent,
+    ProgressComponent,
+    TaskListComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule
